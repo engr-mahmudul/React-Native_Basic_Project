2f3ec7d2da06e158f0cd077d800b3b7f8b0afad9
engr-mahmudul <dev.mahmudulhasan@gmail.com>
2025-02-11 23:57:17 +0200

    TextInput and its values handling

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,4 +1,4 @@
-import {  Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import {  Alert, Image, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
@@ -10,9 +10,13 @@ type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>
 const Details = ({route}:DetailsProps) => {
     const {link} = route.params
   const [message, setMessage] = useState('')
+  const [input, setInput] = useState({a:'',b:''})
   const handlePress = () => {
     setMessage("Am I cute ?")
   };
+  const showInput = () =>{
+    Alert.alert(`A is: ${input.a} and B is: ${input.b}`)
+  }
   return (
     <View style={styles.body}>
       <Text style={styles.textStyle}>Hello Everyone</Text>
@@ -22,6 +26,25 @@ const Details = ({route}:DetailsProps) => {
         source={{uri:link}}/>
       </TouchableOpacity>
       <Text style={{marginTop:30, fontSize:20, color:'gray'}}>{message}</Text>
+      <TextInput 
+      placeholder='Write here something' placeholderTextColor="gray"
+      value={input.a}
+      style={{borderWidth:2, borderColor:'black', borderRadius:20, width:'80%', marginVertical:5}}
+      onChangeText={(text)=>setInput(prevState => ({ ...prevState, a: text }))}
+      />
+      <TextInput 
+      placeholder='Write here something' placeholderTextColor="gray"
+      value={input.b}
+      style={{borderWidth:2, borderColor:'black', borderRadius:20, width:'80%'}}
+      onChangeText={(text)=>setInput(prevState => ({ ...prevState, b: text }))}
+      />
+      <Pressable style={{backgroundColor:'green', borderWidth:2, borderBlockColor:'black', borderRadius:20, width:'80%',padding:10, marginVertical:10}}
+      onPress={()=>showInput()}>
+        <View >
+          <Text style={{color:"white", textAlign:'center'}}> Press me to See your Input</Text>
+        </View>
+        
+      </Pressable>
     </View>
   )
 }
